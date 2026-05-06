953dd2e0462c69e7629dae320bccaa43572a1362
Andreea-Maria <andreea.maria2000@yahoo.com>
2026-05-06 23:07:58 +0300

    setup Room database layer

---
+++ app/src/main/java/com/example/smartpantry/data/local/SmartPantryDatabase.kt
@@ -0,0 +1,14 @@
+package com.example.smartpantry.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [ProductEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class SmartPantryDatabase : RoomDatabase(){
+
+    abstract fun productDao(): ProductDao
+}
