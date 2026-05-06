88295c8231a035c1a2905b4e585d50bfd14fe066
Andreea-Maria <andreea.maria2000@yahoo.com>
2026-05-06 23:49:44 +0300

    setup MVVM home screen with Room integration

--- app/src/main/java/com/example/smartpantry/MainActivity.kt
+++ app/src/main/java/com/example/smartpantry/MainActivity.kt
@@ -4,44 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.smartpantry.data.repository.DatabaseProvider
+import com.example.smartpantry.data.repository.ProductRepository
+import com.example.smartpantry.presentation.home.HomeScreen
+import com.example.smartpantry.presentation.home.ProductViewModel
+import com.example.smartpantry.presentation.home.ProductViewModelFactory
 import com.example.smartpantry.ui.theme.SmartPantryTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
+        val database = DatabaseProvider.getDatabase(this)
+        val repository = ProductRepository(database.productDao())
+
         setContent {
+
             SmartPantryTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+                val viewModel: ProductViewModel = viewModel(
+                    factory = ProductViewModelFactory(repository)
+                )
+
+                HomeScreen(viewModel = viewModel)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SmartPantryTheme {
-        Greeting("Android")
     }
 }
